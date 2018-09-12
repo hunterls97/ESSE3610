0d5303509316551e3b57dc32f3ec786d64d233b0
hunterls97 <hunter.schofield@york.ca>
2018-09-12 08:01:21 -0400

    q3 done

--- a.py
+++ a.py
@@ -1,11 +1,50 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-if __name__ == "__main__":
-	print('ESSE3610 Lab Group 6 - Part A')
+import numpy as np
+import sys
 
+#Group Lab Number
+gln = 6
+
+#define a function to get a rotation matrix about the x-axis
+def Rx(theta):
+	# define the cos and sine variables
+	c,s = np.cos(theta), np.sin(theta)
+
+	#return the rotation matrix
+	return np.matrix([[1, 0, 0],
+										[0, c, s],
+										[0,-s, c]])
+
+#define a function to get a rotation matrix about the y-axis
+def Ry(theta):
+	#define the cos and sine variables
+	c,s = np.cos(theta), np.sin(theta)
+
+	#return the rotation matrix
+	return np.matrix([[c, 0,-s],
+										[0, 1, 0],
+										[s, 0, c]])
+
+#define a function to get a rotation matrix about the z-axis
+def Rz(theta):
+	#define the cos and sine variables
+	c,s = np.cos(theta), np.sin(theta)
+
+	#return the rotation matrix
+	return np.matrix([[c, s, 0],
+										[-s, c, 0],
+										[0, 0, 1]])
+
+#define a function to get the reflection about the y-axis
+def Py():
+	return np.matrix([[1, 0, 0],
+										[0, -1, 0],
+										[0, 0, 1]])
+
+def setup():
 	fig = plt.figure()
 
 	#set figure and axes parameters
@@ -13,26 +52,9 @@ if __name__ == "__main__":
 	ax.set_xlim([-0.02, 0.02])
 	ax.set_ylim([-0.02, 0.02])
 	ax.set_zlim([-0.02, 0.02])
-
-	#define a function to get a rotation matrix about the x-axis
-	def Rx(theta):
-		# define the cos and sine variables
-		c,s = np.cos(theta), np.sin(theta)
-
-		#return the rotation matrix
-		return np.matrix([[1, 0, 0],
-											[0, c, s],
-											[0,-s, c]])
-
-	#define a function to get a rotation matrix about the y-axis
-	def Ry(theta):
-		#define the cos and sine variables
-		c,s = np.cos(theta), np.sin(theta)
-
-		#return the rotation matrix
-		return np.matrix([[c, 0,-s],
-											[0, 1, 0],
-											[s, 0, c]])
+	ax.set_xlabel('x-axis')
+	ax.set_ylabel('y-axis')
+	ax.set_zlabel('z-axis')
 
 	#define the origin point in 3D as a tuple
 	origin = (0,0,0)
@@ -69,3 +91,54 @@ if __name__ == "__main__":
 		plt.pause(.001)
 
 	plt.show()
+
+def q3():
+	fig = plt.figure()
+
+	#set figure and axes parameters
+	ax = fig.gca(projection='3d')
+	ax.set_xlim([-2, 2])
+	ax.set_ylim([-2, 2])
+	ax.set_zlim([-2, 2])
+	ax.set_xlabel('x-axis')
+	ax.set_ylabel('y-axis')
+	ax.set_zlabel('z-axis')
+
+	#define the origin point in 3D as a tuple
+	origin = (0,0,0)
+
+	#define the original cordinates as a set of vectors normal to the xy, yz and xz planes
+	O_norm = np.array([[1,0,0],[0,1,0],[0,0,1]])
+
+	#define the components of the coordinate system
+	X,Y,Z = zip(*O_norm)
+
+	#render the Oxyz
+	ax.quiver(*origin, X, Y, Z, label='rotation')
+
+	#render Ox'y'z'
+	X = np.dot(np.dot(Ry(np.pi / 2), np.dot(Rz(-1 * np.pi / 6), np.dot(Py(), Rx((np.pi * gln / 4))))), X)
+	Y = np.dot(np.dot(Ry(np.pi / 2), np.dot(Rz(-1 * np.pi / 6), np.dot(Py(), Rx((np.pi * gln / 4))))), Y)
+	Z = np.dot(np.dot(Ry(np.pi / 2), np.dot(Rz(-1 * np.pi / 6), np.dot(Py(), Rx((np.pi * gln / 4))))), Z)
+
+	#print((X,Y,Z))
+
+	ax.quiver(*origin, X, Y, Z, label='rotation', color='black')
+
+	plt.show()
+
+if __name__ == "__main__":
+	print('ESSE3610 Lab Group 6 - Part A')
+	
+	#pass 1 as an argument to do the setup/ question 1
+	if len(sys.argv) > 1:
+		if int(sys.argv[1]) == 1:
+			setup()
+
+		if int(sys.argv[1]) == 3:
+			q3()
+
+	else:
+		setup()
+
+	
